3ccf118d0ec3cff4305ff1a59942fac2d6a47543
Henry-0810 <Mun.Li.Pan@students.ittralee.ie>
2022-12-11 14:53:46 +0000

    done

--- Calculations.js
+++ Calculations.js
@@ -4,6 +4,13 @@ for(const input of inputs){
 }
 
 let name = document.getElementById("name");
-if(/\d/.test(name.value)) {
-    alert("Name cannot contain numbers!");
+if(!/^[A-Za-z\s]*$/.test(name.value)) {
+    alert("Only letters and spaces are allowed in a name!");
 }
+
+let email = document.getElementById("email");
+if(!/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email.value)){
+    alert("Wrong email format!");
+}
+
+
