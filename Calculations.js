87a2cddec835bf586dc889a7f9ed5c637b46ba65
Henry-0810 <Mun.Li.Pan@students.ittralee.ie>
2022-12-12 17:26:18 +0000

    done

--- Calculations.js
+++ Calculations.js
@@ -3,14 +3,33 @@ for(const input of inputs){
     input.setAttribute('required',"required");
 }
 
+let totalVal = Number(document.querySelector("#total").value);
+let calcTot = function (e) {
+        if (e.target.checked) {
+            totalVal = Number(e.value);
+        }
+}
+
+let Q1 = document.querySelectorAll('input[name = "ques1"]');
+for (const radioButton of Q1) {
+    document.querySelector("#total").addEventListener('change', calcTot,false);
+}
+
+
+
 let name = document.getElementById("name");
 if(!/^[A-Za-z\s]*$/.test(name.value)) {
-    alert("Only letters and spaces are allowed in a name!");
+    alert("Wrong name format!");
 }
 
 let email = document.getElementById("email");
-if(!/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email.value)){
+const emailRegex = new RegExp(/^[A-Za-z0-9_!#$%&'*+\/=?`{|}~^.-]+@[A-Za-z0-9.-]+$/, "gm");
+const isValidEmail = emailRegex.test(email.value);
+if(!isValidEmail){
     alert("Wrong email format!");
 }
+
+
+
 
 
