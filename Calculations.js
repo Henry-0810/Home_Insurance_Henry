f5076245b38ebe8a33646a5b71f37530961c3e8e
Henry-0810 <Mun.Li.Pan@students.ittralee.ie>
2022-12-12 23:49:26 +0000

    done

--- Calculations.js
+++ Calculations.js
@@ -3,31 +3,58 @@ for(const input of inputs){
     input.setAttribute('required',"required");
 }
 
-let totalVal = Number(document.querySelector("#total").value);
-let calcTot = function (e) {
-        if (e.target.checked) {
-            totalVal = Number(e.value);
+const startingMin = 4;
+let time = startingMin*60;
+
+const countdown = document.getElementById("countdown");
+
+setInterval(updateCount,1000);
+function updateCount(){
+    const min = Math.floor(time/60);
+    let seconds = time%60;
+
+    seconds = seconds < 10? '0' + seconds: seconds;
+    countdown.innerHTML = `${min}:${seconds}`;
+    time--;
+}
+
+let validation = function(){
+    let name = document.getElementById("name");
+    if(!/^[A-Za-z\s]*$/.test(name.value)) {
+        name.focus();
+        name.value = "Incorrect format!";
+    }
+
+    let email = document.getElementById("email");
+    const emailRegex = new RegExp(/^[A-Za-z0-9_!#$%&'*+\/=?`{|}~^.-]+@[A-Za-z0-9.-]+$/, "gm");
+    if(!emailRegex.test(email.value)){
+        email.focus();
+        email.value = "Incorrect format!";
+    }
+
+    let bedroom = document.getElementById("bedrooms");
+    let year = document.getElementById("year");
+    let claims = document.getElementById("yearsClaim");
+    let isDigitValidArr = [bedroom,year,claims];
+    for (let i = 0; i < isDigitValidArr.length; i++) {
+        if(!Number.isInteger(isDigitValidArr[i])){
+            isDigitValidArr[i].focus();
+            isDigitValidArr.value = "Only numbers!";
         }
+    }
 }
 
-let Q1 = document.querySelectorAll('input[name = "ques1"]');
-for (const radioButton of Q1) {
-    document.querySelector("#total").addEventListener('change', calcTot,false);
+let btnCalc = document.getElementById("btnCalc");
+btnCalc.addEventListener('click',validation,false);
+if(name.value ===""){
+    name.style.border = "1px solid Black";
 }
 
 
 
-let name = document.getElementById("name");
-if(!/^[A-Za-z\s]*$/.test(name.value)) {
-    alert("Wrong name format!");
-}
 
-let email = document.getElementById("email");
-const emailRegex = new RegExp(/^[A-Za-z0-9_!#$%&'*+\/=?`{|}~^.-]+@[A-Za-z0-9.-]+$/, "gm");
-const isValidEmail = emailRegex.test(email.value);
-if(!isValidEmail){
-    alert("Wrong email format!");
-}
+
+
 
 
 
