1f65ce0e68030e907e8ebe1d09db307bd33f6215
Henry-0810 <Mun.Li.Pan@students.ittralee.ie>
2022-12-13 15:34:31 +0000

    done

--- Calculations.js
+++ Calculations.js
@@ -1,24 +1,33 @@
-const inputs = document.querySelectorAll('.field');
-for(const input of inputs){
-    input.setAttribute('required',"required");
-}
-
+//countdown timer
 const startingMin = 4;
 let time = startingMin*60;
 
 const countdown = document.getElementById("countdown");
 
-setInterval(updateCount,1000);
+let run = setInterval(updateCount,1000);
 function updateCount(){
     const min = Math.floor(time/60);
     let seconds = time%60;
 
+    if(time <= 0){
+        clearInterval(run);
+        document.getElementById("mainDiv").style.visibility = "hidden";
+        document.getElementById("sessionExpiredID").style.visibility = "visible";
+    }
+
     seconds = seconds < 10? '0' + seconds: seconds;
-    countdown.innerHTML = `${min}:${seconds}`;
+    countdown.innerHTML = `You have ${min}:${seconds} to finish the form`;
     time--;
 }
 
+//validation
 let validation = function(){
+
+    const inputs = document.querySelectorAll('.field');
+    for(const input of inputs){
+        input.setAttribute('required',"required");
+    }
+
     let name = document.getElementById("name");
     if(!/^[A-Za-z\s]*$/.test(name.value)) {
         name.focus();
@@ -26,8 +35,7 @@ let validation = function(){
     }
 
     let email = document.getElementById("email");
-    const emailRegex = new RegExp(/^[A-Za-z0-9_!#$%&'*+\/=?`{|}~^.-]+@[A-Za-z0-9.-]+$/, "gm");
-    if(!emailRegex.test(email.value)){
+    if(!/^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(email.value)){
         email.focus();
         email.value = "Incorrect format!";
     }
@@ -46,9 +54,14 @@ let validation = function(){
 
 let btnCalc = document.getElementById("btnCalc");
 btnCalc.addEventListener('click',validation,false);
-if(name.value ===""){
-    name.style.border = "1px solid Black";
+
+//running total function
+let sumVal = function(){
+
 }
+
+
+
 
 
 
